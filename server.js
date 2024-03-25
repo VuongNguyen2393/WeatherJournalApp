05f6cd48082109898c8285b2888cc28e36467403
vuongnd9 <nguyendinhvuong.act@gmail.com>
2024-03-25 21:58:31 +0700

    initial server code

--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+//get request
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+  });
+
+function listening(){
+  console.log(`App listening on port ${port}`);
+}
+
+//listening
+const server = app.listen(port, listening);
